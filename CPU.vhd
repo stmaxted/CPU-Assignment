846fc86a4293ce43640887048a8bbedbfc7c6db2
Unknown <s.t.maxted@gmail.com>
2018-09-17 09:34:28 +1200

    Skeleton Project Started
    Created a Quartus Prime project for the CPU design. Includes basic skeleton VHD file

---
+++ CPU.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+use IEEE.std_logic_1164;
+use IEEE.numeric_std.all;
+
+entity CPU is
+port (
+			--Port Mapping here
+		);
+end entity CPU;
+
+
+---------------------Functions to implement----------------------------
+--		Conditional Branch
+-- 	Unconditional Branch
+--		Function Call (Jump)
+--		Reading Data from memory
+--		Writing data to memory
+--		Arithmatic or Logic operation
+-------------------------------------------------------------------------
+
+
+
+architecture compute of CPU is
+	--Aliases go here
+begin
+
+process(	--Sensitivity list) is
+	--Variables
+	begin
+		--Outputs <= (others => '-');
+		
+	
+	
+	end process;
+end architecture;
